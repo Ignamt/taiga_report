fdfc197a22ca88d6f38b9e63797c32de6729283d
Ignacio Martinez Tanoira <ignacio.martinez@moorea.io>
2019-06-04 00:42:41 -0300

    Added TaigaAPI class, with tests and _login function

--- taiga_report/classes.py
+++ taiga_report/classes.py
@@ -1,11 +1,16 @@
-"""Classes for the different sections of the report"""
+"""Contains classes for the different sections of the report."""
 import datetime as dt
 
 
 class UserStory:
     """Contains all the US info needed for the report."""
+
     def __init__(self, us):
-        """Sets up attributes for the instance"""
+        """Set up attributes for the instance.
+        
+        Parameters:
+        - us: Dict with all the info that comes from the Taiga
+              API"""
         self.subject = us["subject"]
         self.epic = us["epics"][0]["subject"]
         self.tags = us["tags"]
@@ -19,7 +24,9 @@ class UserStory:
 
 class Epic(list):
     """Gathers the US and task subjects related to a certain epic."""
+
     def __init__(self, subject):
+        """Set up attributes for the instance."""
         super().__init__()
         self.subject = subject
         self.user_stories = list()
@@ -27,15 +34,18 @@ class Epic(list):
 
 class Section:
     """Houses epics and US depending on predefined tags."""
+
     def __init__(self, name):
+        """Set up attributes for the instance."""
         self.name = name.capitalize()
         self.epics = list()
         self.user_stories = list()
 
 
 class Report:
-    """Gathers all sections and prints itself in various formats"""
+    """Gathers all sections and prints itself in various formats."""
+
     def __init__(self, project):
+        """Set up attributes for the instance."""
         self.sections = list()
         self.project = project
-
---
+++ taiga_report/tests/test_taiga_api.py
@@ -0,0 +1,33 @@
+"""Tests for the TaigaAPI"""
+from unittest import TestCase
+from taiga_report import taiga_api
+
+
+class TestAPI(TestCase):
+    """Contains tests for the API class and methods"""
+
+
+    def setUp(self):
+        self.api = taiga_api.TaigaAPI("ignamt-sieel")
+
+
+    def test_api_creation(self):
+        self.assertEqual(self.api.slug, "ignamt-sieel")
+        self.assertEqual(self.api.host,
+                         "https://taiga.leafnoise.io/api/v1/")
+        self.assertEqual(self.api.auth_url,
+                         "https://taiga.leafnoise.io/api/v1/auth")
+        self.assertEqual(self.api.headers,
+                         {"content-type": "application/json",
+                          "x-disable-pagination": "True"})
+
+    def test_login(self):
+        login_data = self.api._login()
+        self.assertEqual(login_data["username"], "ignamt")
+
+    def test_save_auth(self):
+        pass
+
+    
+    def test_auth(self):
+        pass
