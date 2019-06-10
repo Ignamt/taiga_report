71c237f600dd7b6964d6ad7da70474c6324abf8c
Ignacio Martinez Tanoira <ignacio.martinez@moorea.io>
2019-06-09 23:54:09 -0300

    Committing a lot of changes to taiga api. All that is remaining is to get done status to filter for the user stories that are done.

--- taiga_report/taiga_api.py
+++ taiga_report/taiga_api.py
@@ -23,32 +23,100 @@ auth_token = "eyJ1c2VyX2F1dGhlbnRpY2F0aW9uX2lkIjo1MH0:1hW9TL:"\
 
 class TaigaAPI:
     """API class to connect to and interact with the Taiga API."""
-    host = "https://taiga.leafnoise.io/api/v1/"
-    auth_url = host + "auth"
-    headers = {"content-type": "application/json",
-               "x-disable-pagination": "True"}
-    config_file = os.sep.join(["taiga_report", "api.cfg"])
 
     def __init__(self, project_slug):
         """Init TaigaAPI with default attr to specific project."""
-         
+        self.slug = project_slug
+        self.project_id = ""
+        self.config_filename = os.path.join("taiga_report", "api.cfg")
+        self.host = "https://taiga.leafnoise.io/api/v1/"
+        self.auth_url = self.host + "auth"
+        self.headers = {"content-type": "application/json",
+                        "x-disable-pagination": "True"}
         self.login_data = {"type": "normal",
                            "username": "ignamt",
                            "password": "tanoira1"}
-        self.slug = project_slug
-
-        # self._auth()
+        self.authenticated = False
 
     def _login(self):
-        login_data = requests.post(self.auth_url,
-                                   headers=self.headers,
-                                   data=json.dumps(self.login_data))
-        return login_data.json()
+        """Log the api object to Taiga's API.
+
+        RETURNS: a str() of the auth_token
+
+        RAISES:
+            - requests.exceptions.HTTPError if the request fails. (via
+                raise_for_status()
+
+        """
+        response = requests.post(self.auth_url,
+                                 headers=self.headers,
+                                 data=json.dumps(self.login_data))
+
+        # Raises http exception if status_code not ok.
+        response.raise_for_status()
+
+        return response.json()["auth_token"]
 
     def _save_auth(self, auth_token, cfg_json, cfg_file):
         cfg_json["auth_token"] = str(auth_token)
         json.dump(cfg_json, cfg_file)
-        cfg_file.save()
 
     def _auth(self):
-        pass
+        """Add auth_token to request headers or get new one if none stored."""
+        with open(self.config_filename, "r+") as file:
+            cfg_json = json.load(file)
+            file.seek(0)
+            if "auth_token" in cfg_json:
+                print("Using past stored authentication token.")
+                self._add_auth_token(cfg_json["auth_token"])
+                self.authenticated = True
+            else:
+                auth_token = self._login()
+                self._add_auth_token(auth_token)
+                self._save_auth(auth_token=auth_token,
+                                cfg_json=cfg_json,
+                                cfg_file=file)
+                self.authenticated = True
+
+    def _add_auth_token(self, auth_token):
+        self.headers["Authorization"] = "Bearer " + auth_token
+
+    def _project_id(self):
+        if not self.project_id:
+            url = self.host + "projects/by_slug?slug=" + self.slug
+            response = requests.get(url, headers=self.headers)
+            response.raise_for_status()
+            self.project_id = response.json().get("id")
+
+
+
+    def get_user_stories(self):
+        """Get the user stories from the project at the specified slug.
+        
+        RETURNS: List of dicts with all the info about the user stories in the 
+        'DONE' category
+        """
+        if not self.authenticated:
+            self._auth()
+        self._project_id()
+        done_id = self._get_done_status()
+        us_uri = "userstories?project={}&status={}".format(self.project_id,
+                                                           done_id)
+        project_us_url = self.host + us_uri
+        user_stories = requests.get(project_us_url,
+                                    headers=self.headers)
+        user_stories.raise_for_status()
+
+        return user_stories.json()
+        
+
+
+class APIError(Exception):
+    """Exception raised for any errors caused by API.
+
+    If the API is no not responding or is not working as intended.
+    Works like a regular Exception.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
