3d65d8ef0251539950182aba5370ac4a4f57c804
Ignacio Martinez Tanoira <ignacio.martinez@moorea.io>
2019-06-11 00:55:56 -0300

    Changed report structure from objects to json using only UserStory and Report classes. Test suite for inner report structure, developing markdown report printer

--- taiga_report/__main__.py
+++ taiga_report/__main__.py
@@ -1,8 +1,17 @@
-#TODO: Get the user stories from the Taiga API
+"""Executes the report generator and updates the reported User Stories"""
+from taiga_api import TaigaAPI
+from classes import Report, UserStory
 
-#TODO: Create a report instance
+
+api = TaigaAPI("ignamt-sieel")
+us_list = api.download_user_stories()
+
+report = Report("SIEEL")
 
 #TODO: Iterate over US
+for us_json in us_list:
+    us = UserStory(us_json)
+    report.classify_user_story(us)
     #TODO: Check for section tag
         #TODO: Add to section if exists or create if not
 
