2e660097be9088657520171c2173fc3552dbb69e
Ignacio Martinez Tanoira <ignacio.martinez@moorea.io>
2019-06-11 00:52:45 -0300

    Working version of TaigaAPI using my user login credentials with full test suite

--- taiga_report/tests/test_taiga_api.py
+++ taiga_report/tests/test_taiga_api.py
@@ -1,24 +1,21 @@
 """Test for the TaigaAPI."""
 import pytest
-import io
-import json
-import os
-import tempfile
 import requests
 
 from taiga_report import taiga_api
 
+
 @pytest.fixture
 def api():
+    """Return a new api object for each function as needed."""
     return taiga_api.TaigaAPI("ignamt-sieel")
+
 
 def test_api_creation(api):
     """Test the example api is instantiated correctly."""
     assert api.slug == "ignamt-sieel"
     assert api.auth_url == "https://taiga.leafnoise.io/api/v1/auth"
     assert api.host == "https://taiga.leafnoise.io/api/v1/"
-    assert api.headers == {"content-type": "application/json",
-                           "x-disable-pagination": "True"}
 
 
 def test_api_login(api):
@@ -35,23 +32,9 @@ def test_api_login_request_failure(api):
         api._login()
 
 
-def test_api_save_auth(api):
-    """Test if auth_token is saved on a file."""
-    auth_token = "testauthtoken"
-    cfg_file = io.StringIO('{"auth_token": ""}')
-    cfg_json = json.load(cfg_file)
-    api._save_auth(auth_token, cfg_json, cfg_file)
-
-    assert cfg_json["auth_token"] == "testauthtoken"
-
-
 def test_api_auth_with_saved_token(api):
-    """Test if _auth registers auth_token in file object (via mock-file)."""
-    filename = os.path.join(tempfile.gettempdir(), "test_api.cfg")
-    with open(filename, "w") as file:
-        file.write('{"auth_token": "testauthtoken"}')
-
-    api.config_filename = filename
+    """Test if _auth uses same token if already provided."""
+    api.auth_token = "testauthtoken"
     api._auth()
     assert api.headers["Authorization"] == "Bearer testauthtoken"
     assert api.authenticated
@@ -59,22 +42,12 @@ def test_api_auth_with_saved_token(api):
 
 def test_api_auth_without_saved_token(api):
     """Test if _auth saves and registers token."""
-    filename = os.path.join(tempfile.gettempdir(), "test_empty_api.cfg")
-    empty_file = open(filename, "w+")
-    empty_file.write("{}")
-    empty_file.seek(0)
-    assert empty_file.read() == "{}"
-    empty_file.close()
+    api.auth_token = None
+    assert not api.auth_token
 
-    api.config_filename = filename
-    print(api.config_filename)
-    print(api.host)
-    print(api.headers)
-    print(api.login_data)
     api._auth()
     assert api.authenticated
-    with open(filename, "r") as saved_file:
-        assert '"auth_token": "' in saved_file.read()
+    assert api.auth_token
 
 
 def test_add_auth_token(api):
@@ -84,5 +57,31 @@ def test_add_auth_token(api):
     assert api.headers["Authorization"] == "Bearer holaquetal"
 
 
-def test_api_get_user_stories(api):
-    pass
+def test_api_download_user_stories(api):
+    """Test that api gets list of user stories."""
+    userstories = api.download_user_stories()
+    assert isinstance(userstories, list)
+    assert len(userstories)
+
+
+def test_api_download_us_failure_raises_exception(api):
+    with pytest.raises(requests.exceptions.HTTPError):
+        api.host = api.host + "hola/"
+        api.download_user_stories()
+
+
+def test_api_get_done_status_id(api):
+    """Test that api gets the done status id"""
+    done_id = api._get_done_status()
+    assert done_id == 333
+
+
+def test_api_get_done_status_not_found(api):
+    with pytest.raises(requests.exceptions.HTTPError):
+        api.host = api.host+"/hola/"
+        api._get_done_status()
+
+
+def test_api_getting_project_id(api):
+    """Test that api can get project id."""
+    assert api.project_id == 6
