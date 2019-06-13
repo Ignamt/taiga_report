6148e7558a8e3acb49c836f7a88461833d4ffc49
samuel <sbassi@moorea.io>
2019-06-13 13:17:14 -0300

    Added MarkdownPrinter and DocxPrinter.

--- taiga_report/__main__.py
+++ taiga_report/__main__.py
@@ -1,6 +1,6 @@
 """Executes the report generator and updates the reported User Stories"""
 from .taiga_api import TaigaAPI
-from .classes import Report, UserStory
+from .classes import Report, UserStory, MarkdownPrinter, DocxPrinter
 
 
 api = TaigaAPI("ignamt-sieel")
@@ -13,6 +13,9 @@ for us_json in us_list:
     us = UserStory(us_json)
     report.classify_user_story(us)
 
-report.print_markdown()
+#MarkdownPrinter.print_markdown(report)
+DocxPrinter.print_docx(report)
+print("Success :)")
+
 
     
