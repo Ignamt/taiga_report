e060dd3f008c0a712a741164a0751594a3eac2fd
Ignacio Martinez Tanoira <ignacio.martinez@moorea.io>
2019-12-09 21:22:45 -0300

    fix: fixed package ref, added minimum necessary packages to run.

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 import setuptools
 
-with open("requirements.txt", "r") as req:
-    requirements = req.read()
+
+requirements = ["requests",
+                "python-docx"]
+
 
 setuptools.setup(
     name="taiga_report",
     version="0.0.1",
-    packages=["taiga_report.*"],
+    packages=["taiga_report"],
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
-    install_requires = requirements,
-    zip_safe = False,
+    install_requires=requirements,
+    zip_safe=False,
     exclude_package_data={'': ['*.md', '*.docx', '*.yaml', '*.yml', '*tests*', '*tests/*', ]}
 )
